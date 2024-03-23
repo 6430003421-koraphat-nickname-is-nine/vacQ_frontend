563d16023a1f5358e9c3b0193f80a0b477e747ee
koraphat <koraphat.lum@gmail.com>
2024-03-23 22:27:05 +0700

    modify Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,23 @@
 import {Link} from 'react-router-dom'
 
 import {FaQuestionCircle , FaTicketAlt} from 'react-icons/fa'
+
+function Home(){
+    return(
+        <>
+        <section className = 'heading'>
+            <h1>Vac Q: A Vaccine Booking System</h1>
+            <p>Plase choose from an option below</p>
+            <Link to='/new-ticket' className='btn btn-reverse btn-block'>
+                <FaQuestionCircle/>Creat New Appointment
+            </Link>
+
+            <Link to="/tickets" className='btn btn-block'>
+                <FaTicketAlt />View My Appointments
+            </Link>
+        </section>
+        </>
+    )
+}
+
+export default Home;
